f6a25360295f6ee80247dbae2308bc8152af4fef
RNWalker <rebecca.noelia.walker@gmail.com>
2023-08-29 15:24:31 +0100

    add annotations and improve readability of code

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,9 +1,11 @@
+// LOAD IN HTML TAGS
 const form = document.querySelector("#form");
 const button = document.querySelector("#enter");
 const todo = document.querySelector("#new-todo");
 const list = document.querySelector("#list");
 const showDate = document.querySelector("#showDate");
 
+// DECLARE VARIABLES WITH GLOBAL SCOPE
 let item;
 let deleteButton;
 
@@ -16,18 +18,20 @@ todo.addEventListener("input", (event) => {
 button.addEventListener("click", () => {
     const newItem = document.createElement("li");
     newItem.textContent = item;
-   
+ 
+    // CREATE DELETE BUTTON
     let deleteButton = document.createElement("deleteButton");
     deleteButton.innerText = "  Delete";
     
+    // ADD TO LIST
     list.appendChild(newItem);
+    // DELETE FROM LIST
     newItem.appendChild(deleteButton);
 
+    // DECLARE FUNCTION WITHIN FUNCTION
     deleteButton.addEventListener("click", () => {
         list.removeChild(newItem);
    
 })
 
 })
-
-//  Show Date
